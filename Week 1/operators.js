0a1008dedbebd3ddf8e1faf3a18d5a2a68c30e0b
marksikaundi <emmanuelmark117@gmail.com>
2023-04-05 00:04:38 +0200

    operators added

---
+++ Week 1/operators.js
@@ -0,0 +1,18 @@
+/* Arithmetic operators in JS
++ Addition
+- Subtration
+* Multiplication
+/ Division
+ */
+
+/*Comparison operators in JS:
+> greater than
+< less than
+== equal
+*/
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 / 2);
+console.log(2 < 2);
+console.log(2 > 2);
+console.log(2 == 2);
