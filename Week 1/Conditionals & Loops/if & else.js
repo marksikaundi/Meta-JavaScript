f8c502183a15996064506e0f490b7453c8060189
marksikaundi <emmanuelmark117@gmail.com>
2023-04-06 19:47:26 +0200

    conditions and loops

---
+++ Week 1/Conditionals & Loops/if & else.js
@@ -0,0 +1,6 @@
+var results = 50;
+if (results > 40) {
+  console.log("Congraturation you Passed");
+} else {
+  console.log("Unfortunately you did not pass.");
+}
---
+++ Week 1/Conditionals & Loops/if, else if, else.js
@@ -0,0 +1,8 @@
+var food = "hot";
+if (food == "hot") {
+  console.log("Too hot");
+} else if (food == "cold") {
+  console.log("Too cold");
+} else {
+  console.log("just right");
+}
