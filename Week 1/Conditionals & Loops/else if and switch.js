12188938228c6d74ee8577e50b4e0f3c3b9f95cb
marksikaundi <emmanuelmark117@gmail.com>
2023-04-06 22:00:23 +0200

    condition statements

---
+++ Week 1/Conditionals & Loops/else if and switch.js
@@ -0,0 +1,12 @@
+// if, else if, else
+var place = 'Automation'
+
+if (place == 'first'){
+    console.log('Gold')
+} else if (place == 'Second'){
+    console.log('Silver')
+} else if (place == 'third'){
+    console.log('Blozen')
+} else {
+    console.log('No medal')
+}
---
+++ Week 1/Conditionals & Loops/switch.js
@@ -0,0 +1,17 @@
+// switch statement
+var place = "fourth";
+
+switch (place) {
+  case "first":
+    console.log("Gold");
+    break;
+  case "second":
+    console.log("Silver");
+    break;
+  case "third":
+    console.log("Brozen");
+    break;
+
+  default:
+    console.log("No medal");
+}
