27179aedc7917115d1ac907c373ffa110ee6ee3d
marksikaundi <emmanuelmark117@gmail.com>
2023-04-06 13:15:27 +0200

    TASK 4 & 5 completed

---
+++ Week 1/Challenges/Task 4 Add numbers using the + operator.js
@@ -0,0 +1,10 @@
+// Console log the result of adding two numbers, 5 and 10, using the + operator.
+
+// Note: This task should be completed on a single line of code. The output in the console should be 15.
+
+var sum1 = 5 + 10;
+console.log(sum1)
+
+var sum1 = 5;
+var sum2 = 10;
+console.log(sum1 + sum2)
