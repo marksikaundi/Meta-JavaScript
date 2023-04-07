ca89779d08933f92a52eb91b44bde7dbabe8736f
marksikaundi <emmanuelmark117@gmail.com>
2023-04-07 11:06:53 +0200

    Completed tasks

---
+++ Week 1/Challenges/Practice conditional statements.js
@@ -0,0 +1,39 @@
+//First part done
+var age = 10;
+if (age > 65) {
+  console.log("You will get your income from your pension");
+} else if (age < 18) {
+  console.log("Each month you get a salary");
+} else if (age < 18) {
+  console.log("You get an allowance");
+} else {
+  console.log("The value of age is not numerical");
+}
+
+//Second part
+var day = "Sunday";
+switch (day) {
+  case "Monday":
+    console.log("Do something");
+    break;
+
+  case "Tuesday":
+    console.log("Do something");
+    break;
+
+  case "Wednesday":
+    console.log("Do something");
+    break;
+
+  case "Thursday":
+    console.log("Do something");
+    break;
+
+  case "Friday":
+    console.log("Do something");
+    break;
+
+  default:
+    console.log("There is no such day");
+    break;
+}
