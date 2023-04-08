aa0c16d36436630da1d956d586b62aa8665a5f69
marksikaundi <emmanuelmark117@gmail.com>
2023-04-08 11:35:15 +0200

    updates

--- Week 1/Conditionals & Loops/test.js
+++ Week 1/Conditionals & Loops/test.js
@@ -25,6 +25,64 @@ for (var i = 5; i>0; i--){
 }
 console.log('Countdown finished')
 
+//Task 3
+//Write a "while" loop that will perform exactly the same repetitive code as this:
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log('Counting completed!')
+var counter = 1
+
+while (counter < 6){
+    console.log(counter)
+    counter++
+}
+console.log('Counting completed')
+//Note: Name your increment variable i. Update the variable in the while loop using i++.
+
+
+//Task 4
+//Write a "while" loop that will perform exactly the same repetitive code as this:
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+var counter = 5
+
+while (counter > 0){
+    console.log(counter)
+    counter = counter -1
+}
+console.log('Counting completed')
+//Note: In the while loop, decrement the value of i using: i = i - 1.
+
+
+//Task 5
+//Write a "while" loop that will perform exactly the same repetitive code as this:
+// console.log(2018)
+// console.log(2019)
+// console.log(2020)
+// console.log(2021)
+// console.log(2022)
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year++;
+};
+
+
+
+
+
+
+
+
+
+
 
 
 
