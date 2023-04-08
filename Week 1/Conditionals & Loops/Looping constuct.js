ef34a17169aa68cd86aca7f3932bb3387409dde5
marksikaundi <emmanuelmark117@gmail.com>
2023-04-08 09:43:17 +0200

    updates

--- Week 1/Conditionals & Loops/Looping constuct.js
+++ Week 1/Conditionals & Loops/Looping constuct.js
@@ -1,13 +1,25 @@
 // Looping contructs
 //example counting sequence using console.log
 
-console.log(1);
-console.log(2);
-console.log(3);
+console.log(1)
+console.log(2)
+console.log(3)
 
 // Using forloop
-for (var i = 1; 1 < 4; i++) {
-  console.log(i);
+for (var i = 1; 1<4; i++){
+console.log(i)
 }
 
 //KEY var i=0;(set the value) i<3;(specify the condition) i++;(increment the counter)
+
+//While loop
+var i =1
+
+while(i<4){
+console.log(i)
+i=i+1;
+}
+
+
+
+
