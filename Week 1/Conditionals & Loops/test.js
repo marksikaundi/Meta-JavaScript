cfa9c29d0a38e23a7c7020e75d3970062b23c6d6
marksikaundi <emmanuelmark117@gmail.com>
2023-04-08 11:19:24 +0200

    tasks

---
+++ Week 1/Conditionals & Loops/test.js
@@ -0,0 +1,32 @@
+//Task 1
+//Write a "for" loop that will perform exactly the same repetitive code as this:
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log('Counting completed!')
+for (var i = 1; i <= 5; i++){
+    console.log(i)
+}
+console.log('Counting completed!')
+
+
+//Task 2 
+//Write a "for" loop that will perform exactly the same repetitive code as this:
+// console.log(5)
+// console.log(4)
+// console.log(3)
+// console.log(2)
+// console.log(1)
+// console.log('Countdown finished!')
+for (var i = 5; i>0; i--){
+    console.log(i)
+}
+console.log('Countdown finished')
+
+
+
+
+
+
