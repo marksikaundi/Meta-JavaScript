8a3be31b28027fc327f32a5d43a934cb79094646
marksikaundi <emmanuelmark117@gmail.com>
2023-04-08 11:07:10 +0200

    updates

---
+++ Week 1/Conditionals & Loops/Whileloop.js
@@ -0,0 +1,9 @@
+//while loop will keep looping through a code 
+//block as long as a specified condition is true. 
+var counter = 6;
+
+while (counter > 0) {
+  console.log(counter);
+  counter = counter - 1;
+}
+console.log("Keep going with the same progress!🔥");
