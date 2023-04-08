05b1b45e90a043304665df2ed2216e240ccfa001
marksikaundi <emmanuelmark117@gmail.com>
2023-04-08 11:49:24 +0200

    nested loop

---
+++ Week 1/Conditionals & Loops/Nested loop.js
@@ -0,0 +1,10 @@
+//Nested loop Monday to Friday
+for (var i = 1; i <= 2; i++) {
+  for (var j = 1; j <= 5; j++) {
+    console.log("Week " + i + " day " + j);
+  }
+}
+
+for (var year = 2023; year < 2035; year++) {
+  console.log(year);
+}
