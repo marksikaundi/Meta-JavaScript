8bd4a84c253c8ce2c5a0194aa61d6fe6a69b0b59
marksikaundi <emmanuelmark117@gmail.com>
2023-04-08 11:56:01 +0200

    Updated coments in the loop

--- Week 1/Conditionals & Loops/Nested loop.js
+++ Week 1/Conditionals & Loops/Nested loop.js
@@ -9,7 +9,7 @@ for (var year = 2023; year < 2025; year++) {
   console.log(year);
 }
 
-//ADDED NESTED LOOP
+//ADDED NESTED LOOP INSIDE THE FORLOOP TO DISPLAY YEARS & MONTHS
 for (var year = 2023; year < 2025; year++) {
   console.log(year);
   for (var month = 6; month < 9; month++) {
