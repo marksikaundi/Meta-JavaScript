a8a5471c9bb723b9d0ed2381f46864887e46c0e9
marksikaundi <emmanuelmark117@gmail.com>
2023-04-08 17:28:14 +0200

    self check quiz

---
+++ Week 1/Challenges/test.js
@@ -0,0 +1,57 @@
+var i = 3;
+if (i < 5) {
+  console.log("Hello");
+} else {
+  console.log("Goodbye");
+}
+
+var i = 1;
+if (i == 0 && i == 1) {
+  console.log("Hello");
+} else {
+  console.log("Goodbye");
+}
+
+for (i = 0; i < 2; i++) {
+  console.log("Hello");
+}
+
+var i = 0;
+while (i < 3) {
+  console.log("Hello");
+  i++;
+}
+
+for (i = 0; i < 2; i++) {
+  for (var j = 0; j < 3; j++) {
+    console.log("Hello");
+  }
+}
+var i = 7;
+if (i <= 5) {
+  console.log("Hello");
+} else if (i <= 10) {
+  console.log("Goodnight");
+} else {
+  console.log("Goodbye");
+}
+
+var i = 3;
+switch (i) {
+  case 1:
+    console.log("Hello");
+    break;
+  case 2:
+    console.log("Goodnight");
+    break;
+  case 3:
+    console.log("Goodbye");
+    break;
+}
+
+var i = 3;
+if (i == 2 || i == 3) {
+  console.log("Hello");
+} else {
+  console.log("Goodbye");
+}
