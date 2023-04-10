24c51a48a5989768ef2c516a6ce20e23d9410eb7
marksikaundi <emmanuelmark117@gmail.com>
2023-04-10 21:30:34 +0200

    uppda

--- Week 2/Array, objects & function/Func.js
+++ Week 2/Array, objects & function/Func.js
@@ -1,8 +1,14 @@
-function addTwoNums(){
-    var a = 300
-    var b = 100
-    
-    var c = a + b
-    
-    console.log(c)
-    }
+//Function
+function addTwoNums() {
+  var a = 300;
+  var b = 100;
+
+  var c = a + b;
+
+  console.log(c);
+}
+
+//Array Literal syntax
+var category1 = ["Horror", "Scifi", "Romance", "Action"];
+
+console.log(category1[2]);
