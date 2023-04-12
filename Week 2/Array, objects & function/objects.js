e906aa4a0a3b929ee20690333d2cfc7a816900b8
marksikaundi <emmanuelmark117@gmail.com>
2023-04-12 13:20:14 +0200

    ob

---
+++ Week 2/Array, objects & function/objects.js
@@ -0,0 +1,17 @@
+//using keywords notation
+var storeManager = {};
+storeManager.rangeTilesPerTurn = 4;
+storeManager.socialSkills = 50;
+storeManager.streetSmarts = 50;
+storeManager.health = 30;
+storeMnager.specialAbiliyt = "finding business oppoertunities";
+storeManager.greetings = "Lets nake some money";
+
+/*
+		        Property value
+                    |
+    socialSkils = 50;
+        |
+property key
+
+*/
