0706bb9698d2044de8aa32c56f126eb30df67fd6
marksikaundi <emmanuelmark117@gmail.com>
2023-04-13 09:43:43 +0200

    added math.random()

---
+++ Week 2/Array, objects & function/MathRandom.js
@@ -0,0 +1,13 @@
+//Math.random()
+
+// Generate a decimal nuber between 0 and 0.99
+Math.random();
+
+//Save it to a variable
+var decimal = Math.random();
+
+//Log the value of decimal to the console
+console.log(decimal);
+
+//Log the value of decimal MULTIPLIED BY10
+console.log(decimal * 10);
