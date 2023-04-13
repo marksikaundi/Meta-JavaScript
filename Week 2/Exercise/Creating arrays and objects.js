1ef1ff356b33b4a16a2a9f944fee3f54bd0ca885
marksikaundi <emmanuelmark117@gmail.com>
2023-04-13 10:24:30 +0200

    quiz added

---
+++ Week 2/Exercise/Creating arrays and objects.js
@@ -0,0 +1,81 @@
+// Exercise: Creating arrays and objects
+// In this exercise lab you will practice creating arrays and objects.
+
+// Tasks to complete
+// Create a new empty array literal and assign it to the variable clothes.
+var clothes = [];
+
+// Add 5 of your favorite items of clothing as strings using the push() method.
+var clothes = [];
+clothes.push('gray t-shirt'); // 1st item of clothing
+clothes.push('blue t-shirt'); // 2nd item of clothing
+clothes.push('yellow t-shirt'); // 3rd item of clothing
+clothes.push('slippers'); // 4th item of clothing
+clothes.push('old jeans'); // 5th item of clothing
+
+// Remove the fifth piece of clothing from the array using the pop() method.
+var clothes = [];
+clothes.push('gray t-shirt'); // 1st item of clothing
+clothes.push('blue t-shirt'); // 2nd item of clothing
+clothes.push('yellow t-shirt'); // 3rd item of clothing
+clothes.push('slippers'); // 4th item of clothing
+clothes.push('old jeans'); // 5th item of clothing
+clothes.pop();
+
+// Add a new piece of clothing using the push() method.
+var clothes = [];
+clothes.push('gray t-shirt'); // 1st item of clothing
+clothes.push('blue t-shirt'); // 2nd item of clothing
+clothes.push('yellow t-shirt'); // 3rd item of clothing
+clothes.push('slippers'); // 4th item of clothing
+clothes.push('old jeans'); // 5th item of clothing
+clothes.pop();
+clothes.push('green scarf');
+
+// Use console.log to show the third item from the clothes array in the console.
+var clothes = [];
+clothes.push('gray t-shirt'); // 1st item of clothing
+clothes.push('blue t-shirt'); // 2nd item of clothing
+clothes.push('yellow t-shirt'); // 3rd item of clothing
+clothes.push('slippers'); // 4th item of clothing
+clothes.push('old jeans'); // 5th item of clothing
+clothes.pop();
+clothes.push('green scarf');
+console.log(clothes[2]);
+
+// Create a new empty object literal and assign it to the variable favCar.
+var favCar = {};
+
+
+// Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+var favCar = {};
+favCar.color = "red";
+
+// Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+var favCar = {};
+favCar.color = "red";
+favCar.convertible = true;
+
+// Use the console to log the entire favCar object.
+var favCar = {};
+favCar.color = "red";
+favCar.convertible = true;
+console.log(favCar);
+
+
+// Tips
+// Remember to use the object literal syntax: {}.
+
+// Remember to use the array literal syntax: [].
+
+// Resources
+// Video (Conceptual): Arrays
+
+
+// Video (Mix): Introduction to Arrays
+
+
+// Video (Conceptual): Objects
+
+
+// Video (Mix): Objects
