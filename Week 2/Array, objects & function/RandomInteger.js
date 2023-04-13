5ac0f8563d9b3efd1e701dd497b4bec03e620eea
marksikaundi <emmanuelmark117@gmail.com>
2023-04-13 09:57:11 +0200

    randominteger()

---
+++ Week 2/Array, objects & function/RandomInteger.js
@@ -0,0 +1,10 @@
+//Random integer
+
+//get a random decimal number between 0 and 0.099 and save it to a variable
+var decimal = Math.random() * 10;
+
+//rounding up the value of decimal
+var rounded = Math.ceil(decimal);
+
+//log the value of rounded to the console
+console.log(rounded);
