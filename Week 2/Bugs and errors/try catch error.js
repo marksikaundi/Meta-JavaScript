8577a65da06a1a3ab4633b4f83f482853ad91e0a
marksikaundi <emmanuelmark117@gmail.com>
2023-04-15 08:58:21 +0200

    added error handling

---
+++ Week 2/Bugs and errors/try catch error.js
@@ -0,0 +1,10 @@
+//Try catch block
+
+try {
+  console.log(fluit1 + fluit2);
+} catch (error) {
+  console.log(err);
+  console.log("There was an error");
+  console.log("The error was saved in the error log");
+}
+console.log("My program does not stop");
