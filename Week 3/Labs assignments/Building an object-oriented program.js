04555805472d025bb9e71785aa0dccf25ff0ab52
marksikaundi <emmanuelmark117@gmail.com>
2023-04-29 09:59:36 +0200

    done with task 3

--- Week 3/Labs assignments/Building an object-oriented program.js
+++ Week 3/Labs assignments/Building an object-oriented program.js
@@ -29,3 +29,10 @@ class Worker extends Person {
     this.xp += 10;
   }
 }
+
+//Task 3: Code an intern object, run methods
+function intern() {
+  var worker = new Worker(0, 10, "Bob", 21, 110);
+  worker.gotToWork();
+  return Worker;
+}
