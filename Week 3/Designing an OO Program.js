1529516049d9893ba90b0b6724ab9c26fd9ec311
marksikaundi <emmanuelmark117@gmail.com>
2023-04-29 09:16:41 +0200

    Animal object

--- Week 3/Designing an OO Program.js
+++ Week 3/Designing an OO Program.js
@@ -104,8 +104,28 @@ class Parrot extends Bird {
 // Coding the Animal class
 // First, I'll code the base Animal class
 
-
-
+class Animal {
+  constructor(color = "yellow", energy = 100) {
+    this.color = color;
+    this.energy = energy;
+  }
+  isActive() {
+    if (this.energy > 0) {
+      this.energy -= 20;
+      console.log("Energy is decreasing, currently at:", this.energy);
+    } else if (this.energy == 0) {
+      this.sleep();
+    }
+  }
+  sleep() {
+    this.energy += 20;
+    console.log("Energy is increasing, currently at:", this.energy);
+  }
+  getColor() {
+    console.log(this.color);
+  }
+}
+//Each animal object, no matter which one it is, will share the properties of color and energy.
 
 
 
