d7a9b52c4fef37a0ae6f6bb7e00923ec96d4b3f3
marksikaundi <emmanuelmark117@gmail.com>
2023-04-29 09:39:34 +0200

    defult params

--- Week 3/Labs assignments/Building an object-oriented program.js
+++ Week 3/Labs assignments/Building an object-oriented program.js
@@ -1,3 +1,7 @@
 class Person {
-   constructor(name="Mark",age="23",energy=100) 
+   constructor(name="Mark",age="23",energy=100) {
+    this.name=name;
+    this.age=age;
+    this.energy=energy;
+   } 
 }
