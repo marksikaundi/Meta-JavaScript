d890d91e12da58e1c1fb52d94bbe62d8586d399e
marksikaundi <emmanuelmark117@gmail.com>
2023-04-30 12:53:45 +0200

    arrays are iterable

--- Week 3/Advanced Javascript/For of loops and objects.js
+++ Week 3/Advanced Javascript/For of loops and objects.js
@@ -1,8 +1,19 @@
-const car = {
-    speed: 200,
-    color: "black"
-}
+// const car = {
+//     speed: 200,
+//     color: "black"
+// }
 
-for(prop of car){
-    console.log(prop)
+// for(prop of car){
+//     console.log(prop)
+// }
+
+const colors = ["red", "orange", "yellow"];
+for (var color of colors) {
+  console.log(color);
 }
+//This time, the output is as follows:  red, orange and yello
+
+
+
+
+
