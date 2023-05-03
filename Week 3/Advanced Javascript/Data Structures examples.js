310724ce1888b13e467ae50a09f49822fc31b91c
marksikaundi <emmanuelmark117@gmail.com>
2023-05-03 07:44:18 +0200

    DA

---
+++ Week 3/Advanced Javascript/Data Structures examples.js
@@ -0,0 +1,48 @@
+// In this reading, you'll learn about some of the most common examples of data structures.
+
+// The focus will be on working with the Object, Array, Map and Set data structures in JavaScript, through a series of examples.
+
+// Note that this reading will not include a discussion of some data structures that exist in some other languages, such as Queues or Linked Lists.  Although these data structures (and other data structures too!) can be custom-coded in JavaScript, the advanced nature of these topics and the difficulty with implementing related exercises means they are beyond the scope of this reading. 
+
+// Working with arrays in JavaScript
+// Previously, you've covered a lot of concepts related to how to work with JavaScript arrays.
+
+// However, there are still a few important topics that can be covered, and one of those is, for example, working with some built-in methods.
+
+// In this reading, the focus is on three specific methods that exist on arrays:
+
+//  forEach 
+
+// filter
+
+//  map
+
+//  Let's explore these methods.
+
+// The forEach() method
+// Arrays in JavaScript come with a handy method that allows you to loop over each of their members. 
+
+// Here's the basic syntax:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
