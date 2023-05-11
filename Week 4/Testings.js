57ea43b3aa8677e18e9fe5aa3ebaeac2249acbad
marksikaundi <emmanuelmark117@gmail.com>
2023-05-11 20:44:27 +0200

    unit testing

--- Week 4/Testings.js
+++ Week 4/Testings.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
-export default function Testings() {
-  return (
-    <div>
-      Unit testing
-    </div>
-  )
+// Task 1: Code the timesTwo function declaration
+function timesTwo(a) {
+    return a * 2;
 }
+
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
+
+
+
+///Test
+const timesTwo = require('./timesTwo');
+
+// Write the first test
+test('returns the number times 2', () => {
+    expect(timesTwo(10)).toBe(20)
+});
