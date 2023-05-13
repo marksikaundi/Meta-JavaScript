5e6184c09d2284711b5b5d8df2d9da70afc18cfa
marksikaundi <emmanuelmark117@gmail.com>
2023-05-13 17:07:49 +0200

    completed

---
+++ Week 5/Final graded quiz Programming with JavaScript/index.js
@@ -0,0 +1,66 @@
+
+// const a = true;
+// if(!a) {
+//     console.log("Green");
+// } else {
+//     console.log("Blue");
+// }
+
+
+// var message = "Hello";
+// message += " World!";
+// message = "Goodbye!";
+// console.log(message);
+
+
+// var x = 10;
+
+// if(x > 10) {
+//     console.log("Apple");
+// } else if(x > 5) {
+//     console.log("Pear");
+// } else {
+//     console.log("Orange");
+// }
+
+
+
+// var result = 0;
+
+// var i = 0;
+// var limit = 3;
+// while(i < limit) {
+//     result += 2;
+//     i++;
+// }
+
+// console.log(result);
+
+
+
+// var result;
+// console.log(result);
+// result = 7;
+
+
+// var result;
+// console.log(result);
+// result = 7;
+
+// // The function declaration is missing a name. It should be named something like:
+
+// // function addNumbers(a, b) { return a + b; }
+
+
+// var cat = {}
+// cat.sound = "meow"
+// var catSound = "purr"
+// console.log(cat.sound)
+
+
+
+function addFive(val) {
+    return val + 5;
+  };
+  module.exports = addFive;
+  
